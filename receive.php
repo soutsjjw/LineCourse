70d6c863e96318868f93a0466b36bf26e11ec294
soutsjjw <souts.jjw@gmail.com>
2018-11-19 13:09:27 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -7,21 +7,22 @@
   
   $sender_userid = $json_obj->events[0]->source->userId; //取得訊息發送者的id
   $sender_txt = $json_obj->events[0]->message->text; //取得訊息內容
+  $sender_replyToken = $json_obj->events[0]->replyToken; //取得訊息的replyToken
   
   $response = array (
-    "to" => $sender_userid,
+    "replyToken" => $sender_replyToken,
     "messages" => array (
       array (
         "type" => "text",
-        "text" => "Hello. You say ". $sender_txt
+        "text" => "Hello. You say". $sender_txt
       )
     )
   );
   
  fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
   $header[] = "Content-Type: application/json";
-  $header[] = "Authorization: Bearer wkUgr5DF+piiijypDbcz/GCipMJj8YX0ESW6ckRdKOC70L6rBJZOLxXgBftU/MTW45+NcuGcsFtwqczSUsxFn4/TouNukyVCmob4ZhB3DGnbE3ebuwQQtfCBBSY21JPwQKqS05LPV/HYnfQEA0TkfQdB04t89/1O/w1cDnyilFU=";
-  $ch = curl_init("https://api.line.me/v2/bot/message/push");
+  $header[] = "Authorization: Bearer gd1gyH+Pc5TROu9ku5u/5tDvFnffsU8nXU69zXuhTgE0dIS5nVGmx9Js8PwijeUqgFuwWXzyJ14/N5FUmp/UXsmSJbUsxMGA6AW1gozlf6cbEgSGLiC02BEaRa5wUSqE7df8FOANP1WjPW8Mh/TgtwdB04t89/1O/w1cDnyilFU=";
+  $ch = curl_init("https://api.line.me/v2/bot/message/reply");
   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
   curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
